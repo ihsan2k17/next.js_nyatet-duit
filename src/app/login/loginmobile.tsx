62e75004fc80686a28896ed9be87c6c845235f84
ihsan <ihsan2870728@gmail.com>
2025-08-14 10:31:10 +0700

    commit lagi

--- src/app/login/loginmobile.tsx
+++ src/app/login/loginmobile.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import Image from "next/image"
 import bgimg from "../../../public/bg.jpg";
+import paperplane from "../../../public/paper-plane-freepik.png";
 import LoginModal from "@/component/modalLogin";
+import { IoCloseCircle } from "react-icons/io5";
+import { MdKeyboardBackspace } from "react-icons/md";
 export default function LoginMobile() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
@@ -28,10 +32,23 @@ export default function LoginMobile() {
                     z-0 
                     opacity-20" 
                 style={{backgroundImage:`url(${bgimg.src})`}}/>
+            {modalLogin && (
+                <button className=" absolute top-10 left-5 z-20" onClick={() => setModalLogin(false)}>
+                    <MdKeyboardBackspace className=" text-button-primary" size={30}/>
+                </button>
+            )}
+            <h1 className={`text-3xl pl-2 font-[1000] text-start w-full 
+                ${!modalLogin ? "w-[-10]" : "w-0"} font-sans text-button-secondary`}>Welcome, Brokk!!</h1>
+            {!modalLogin && (
+                <div className="flex-col flex z-50 p-10 justify-center">
+                    <Image src={paperplane} alt="paper plane for freepik" width={100} height={100} className="w-lg h-max" priority/>
+                    <h1 className="text-4xl font-black text-button-primary/80 font-dancing-script">Nyatet Duit</h1>
+                </div>
+            )} 
             {!modalLogin && (
                 <button
                     onClick={() => setModalLogin(true)}
-                    className="px-20 py-2 bg-button-primary text-white rounded-full mb-4"
+                    className="px-30 py-2 bg-button-primary text-white rounded-full mb-4"
                 > Login
                 </button>
             )}
@@ -41,7 +58,7 @@ export default function LoginMobile() {
 
                 </div>
             </LoginModal>
-            <h1>Login - Mobile</h1>
+            <h1 className="font-black text-xl">Login - Mobile</h1>
         </div>
     );
 }
--- src/component/modalLogin.tsx
+++ src/component/modalLogin.tsx
@@ -9,14 +9,10 @@ interface ILoginModal {
 const LoginModal = ({children, ModalLogin, Close}: ILoginModal) => {
     if(!ModalLogin) return null;
     return (
-        <div className="relative inset-0 z-50 flex w-full p-2">
-            <button className="absolute top-0 right-0" onClick={Close}>
-                <IoCloseCircle size={30}/>
-            </button>
+        <div className="relative inset-0 flex w-full p-2">
             <form className="flex 
                 flex-col 
-                z-10 
-                gap-4 w-full pt-8 pb-8 pr-4 pl-4 bg-secondary/30 rounded-3xl border-2 border-button-primary">
+                gap-4 w-full pt-8 pb-8 pr-4 pl-4 bg-secondary/30 rounded-3xl border-2 border-button-primary z-10 relative">
                     {children}
             </form>
         </div>
